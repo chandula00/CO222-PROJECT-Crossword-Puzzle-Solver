e90d8e1beda981225de6f4b257c535bbe7572661
Chandula Adhikari <e19008@eng.pdn.ac.lk>
2023-03-03 00:08:02 +0530

    Update puzzle-static.c

--- puzzle-static.c
+++ puzzle-static.c
@@ -14,6 +14,8 @@ Topic       : Crossword Puzzle Solver
 // Header file that declares several functions that are useful for testing and mapping characters.(toupper())
 #include <ctype.h>
 
+#include <time.h>
+
 /*
 At the initial point we don't know the sizes of the arrays to be created.
 Hence, suggesting an adequate fine value for size
@@ -23,9 +25,12 @@ Hence, suggesting an adequate fine value for size
 // Creating a collection of commonly used variables under a single name. (This make it easy to create and return several variables at once.)
 typedef struct possible
 {
-    int occurances; // How many times did any event occured
-    int column;     // Which column
-    int row;        // Which row
+    int occurances;      // How many times did any event occured
+    int best_occurances; // How many times did any more appropriate event occured
+    int column;          // Which column
+    int best_column;
+    int row; // Which row
+    int best_row;
 } possible;
 
 char grid[Size][Size]; // Declare a 2D array of size 100x100 to store puzzle grid as global
@@ -36,7 +41,7 @@ void print_puzzle(int count)
     for (int row = 0; row < count; row++) // Call row by row
     {
         printf(grid[row]); // print the row
-        putchar('\n'); // Going to a new line
+        putchar('\n');     // Going to a new line
     }
 }
 
@@ -47,6 +52,21 @@ void fill_horizontal(char *word, possible horizontal)
     int column = horizontal.column;
     int row = horizontal.row;
 
+    int j = 0; // Declare a int variable to represent the index values of characters in the word
+    for (int i = column; i < column + strlen(word); i++)
+    {
+        grid[row][i] = word[j]; // Filling the puzzle grid character by character
+        j++;                    // Changing to next index
+    }
+}
+
+// Function to fill the specific word horizontally in the best place
+void fill_horizontal_best(char *word, possible horizontal)
+{
+
+    // Row and Column values of the starting posithion
+    int column = horizontal.best_column;
+    int row = horizontal.best_row;
     int j = 0; // Declare a int variable to represent the index values of characters in the word
     for (int i = column; i < column + strlen(word); i++)
     {
@@ -70,6 +90,21 @@ void fill_vertical(char *word, possible vertical)
     }
 }
 
+// Function to fill the specific word vertically in the best place
+void fill_vertical_best(char *word, possible vertical)
+{
+    // Row and Column values of the starting posithion
+    int column = vertical.best_column;
+    int row = vertical.best_row;
+
+    int j = 0; // Declare a int variable to represent the index values of characters in the word
+    for (int i = row; i < row + strlen(word); i++)
+    {
+        grid[i][column] = word[j]; // Filling the puzzle grid character by character
+        j++;                       // Changing to next index
+    }
+}
+
 // Function to check if there are any appropriate slots for the word in horizontal and count how many set of slots are there
 possible check_horizontal(char *word, int row_count)
 {
@@ -77,10 +112,11 @@ possible check_horizontal(char *word, int row_count)
     Parameters : char *word    >>>> pointer to the selected word
                  int row_count >>>> number of rows in the puzzle grid
     */
-    int column, row;           // Declare two integer variables to represent rows and colums
-    int slots_for_word = 0;    // Declare a integer variable for count of slots
-    possible horizontal;       // Declare s set of new horizontal variables
-    horizontal.occurances = 0; // variable to count event occurances
+    int column, row;                        // Declare two integer variables to represent rows and colums
+    int slots_for_word = 0, best_slots = 0; // Declare two integer variable for count of slots and best slots
+    possible horizontal;                    // Declare s set of new horizontal variables
+    horizontal.occurances = 0;              // variable to count event occurances
+    horizontal.best_occurances = 0;
 
     for (row = 0; row < row_count; row++) // Calling row by row
     {
@@ -90,25 +126,50 @@ possible check_horizontal(char *word, int row_count)
             // Check whether if the character is fillable or it is the character that we trying to assign
             if (grid[row][column] == '#' || toupper(grid[row][column]) == toupper(word[slots_for_word]))
             {
+
+                if (toupper(grid[row][column]) == toupper(word[slots_for_word]))
+                {
+                    best_slots++; // Counting it as a best slot
+                }
                 slots_for_word++; // Counting it as a possible slot
             }
             else
             {
-                if (slots_for_word == strlen(word)) // Checking number of possible slots equal to the length of the selected word
+                if (slots_for_word == strlen(word) && best_slots >= 1)
+                {
+                    horizontal.best_occurances++;                   // Counting event occurences
+                    horizontal.best_column = column - strlen(word); // Starting column value of the possible slot
+                    horizontal.best_row = row;                      // Starting row value of the possible slot
+                    horizontal.occurances++;
+                    horizontal.column = column - strlen(word);
+                    horizontal.row = row;
+                }
+                else if (slots_for_word == strlen(word)) // Checking number of possible slots equal to the length of the selected word
                 {
                     horizontal.occurances++;                   // Counting event occurences
                     horizontal.row = row;                      // Starting row value of the possible slot
                     horizontal.column = column - strlen(word); // Starting column value of the possible slot
                 }
+                best_slots = 0;
                 slots_for_word = 0; // Possible slot numbers set to 0
             }
         }
         // When above loop ended with it limits while satisfying conditions
-        if (slots_for_word == strlen(word))
+        if (slots_for_word == strlen(word) && best_slots >= 1)
         {
+            horizontal.best_occurances++;                   // Counting event occurences
+            horizontal.best_column = column - strlen(word); // Starting column value of the possible slot
+            horizontal.best_row = row;                      // Starting row value of the possible slot
+
             horizontal.occurances++;
-            horizontal.row = row;
             horizontal.column = column - strlen(word);
+            horizontal.row = row;
+        }
+        else if (slots_for_word == strlen(word)) // Checking number of possible slots equal to the length of the selected word
+        {
+            horizontal.occurances++;                   // Counting event occurences
+            horizontal.row = row;                      // Starting row value of the possible slot
+            horizontal.column = column - strlen(word); // Starting column value of the possible slot
         }
     }
 
@@ -122,10 +183,11 @@ possible check_vertical(char *word, int row_grid)
     Parameters : char *word    >>>> pointer to the selected word
                  int row_count >>>> number of rows in the puzzle grid
     */
-    int column, row;         // Declare two integer variables to represent rows and colums
-    int slots_for_word = 0;  // Declare a integer variable for count of slots
-    possible vertical;       // Declare s set of new horizontal variables
-    vertical.occurances = 0; // variable to count event occurances
+    int column, row;                        // Declare two integer variables to represent rows and colums
+    int slots_for_word = 0, best_slots = 0; // Declare two integer variable for count of slots and best slots
+    possible vertical;                      // Declare s set of new horizontal variables
+    vertical.occurances = 0;                // variable to count event occurances
+    vertical.best_occurances = 0;
 
     for (column = 0; column < strlen(grid[0]); column++) // Calling column by column
     {
@@ -134,25 +196,51 @@ possible check_vertical(char *word, int row_grid)
         {
             if (grid[row][column] == '#' || toupper(grid[row][column]) == toupper(word[slots_for_word])) // Checking number of possible slots equal to the length of the selected word
             {
-                slots_for_word++; // Counting it as a possible slot
+                // Counting it as a possible slot
+                if (toupper(grid[row][column]) == toupper(word[slots_for_word]))
+                {
+                    best_slots++; // Counting it as a best slot
+                }
+                slots_for_word++;
             }
             else
             {
-                if (slots_for_word == strlen(word)) // Checking number of possible slots equal to the length of the selected word
+                if (slots_for_word == strlen(word) && best_slots >= 1) // Checking number of possible slots equal to the length of the selected word and is this athe appropriate one
+                {
+
+                    vertical.best_occurances++;             // Counting event occurences
+                    vertical.best_column = column;          // Starting column value of the possible slot
+                    vertical.best_row = row - strlen(word); // Starting row value of the possible slot
+                    vertical.occurances++;
+                    vertical.column = column;
+                    vertical.row = row - strlen(word);
+                }
+                else if (slots_for_word == strlen(word)) // Checking number of possible slots equal to the length of the selected word
                 {
                     vertical.occurances++;             // Counting event occurences
                     vertical.column = column;          // Starting column value of the possible slot
                     vertical.row = row - strlen(word); // Starting row value of the possible slot
                 }
+                best_slots = 0;
                 slots_for_word = 0; // Possible slot numbers set to 0
             }
         }
         // When above loop ended with it limits while satisfying conditions
-        if (slots_for_word == strlen(word))
+        if (slots_for_word == strlen(word) && best_slots >= 1) // Checking number of possible slots equal to the length of the selected word and is this the appropriate one
         {
+            vertical.best_occurances++;             // Counting event best occurences
+            vertical.best_column = column;          // Starting column value of the possible slot
+            vertical.best_row = row - strlen(word); // Starting row value of the possible slot
+            best_slots = 0;
             vertical.occurances++;
             vertical.column = column;
             vertical.row = row - strlen(word);
+        }
+        else if (slots_for_word == strlen(word)) // Checking number of possible slots equal to the length of the selected word
+        {
+            vertical.occurances++;             // Counting event occurences
+            vertical.column = column;          // Starting column value of the possible slot
+            vertical.row = row - strlen(word); // Starting row value of the possible slot
         }
     }
 
@@ -162,6 +250,9 @@ possible check_vertical(char *word, int row_grid)
 // Main function
 int main()
 {
+    // double time_spent = 0.0;
+    // clock_t begin = clock();
+
     char word[Size][Size]; // Declare a 2D array of size 100x100 to store words
 
     // Declare two integer variables for row values
@@ -187,7 +278,9 @@ int main()
         }
     }
 
-    fflush(stdin); // Clear whatever entered before using std input
+    // fflush(stdin);
+    char t1;
+    scanf("%c", &t1); // Clear the newline character entered before using std input
 
     // 'for loop' to get words line by line
     for (; scanf("%[^\n]%*c", word[row_word]) == 1; row_word++)
@@ -203,6 +296,7 @@ int main()
                 printf("INVALID INPUT"); // Print
                 exit(0);                 // Exit from the program
             }
+            // word[row_word][column_word] = toupper(word[row_word][column_word]); // Convert all to capitals
         }
     }
 
@@ -211,7 +305,7 @@ int main()
 
     int checking = 0; // Declare variable for puzzle grid check count
 
-    while (checking < 3) // Check three times if there only one suitable place for a single word
+    while (checking < word_count) // Check multiple times if there only one suitable place for a single word
     {
         // Checking word by word
         for (int i = 0; i < word_count; i++)
@@ -222,27 +316,33 @@ int main()
             vertical = check_vertical(word[i], row_count);     // Check suitable slots in vertical direction
             horizontal = check_horizontal(word[i], row_count); // Check suitable slots in horizontal direction
 
-            // When there is only one suitable horizontal space that fit for the word
-            if (horizontal.occurances == 1 && vertical.occurances != 1)
+            // When there is only one most suitable horizontal space that fit for the word
+            if (horizontal.best_occurances > 0)
             {
-                fill_horizontal(i, horizontal); // Fill the puzzle grid
-            }
-            // When there is only one suitable vertical space that fit for the word
-            else if (vertical.occurances == 1 && horizontal.occurances != 1)
-            {
-                fill_vertical(i, vertical);
+                fill_horizontal_best(word[i], horizontal); // Fill the puzzle grid
             }
 
-            // When there is no any suitable place for the word
-            else if (horizontal.occurances + vertical.occurances == 0)
+            // When there is only one most suitable vertical space that fit for the word
+            else if (vertical.best_occurances > 0)
             {
-                printf("IMPOSSIBLE\n"); // Print filling puzzle grid is IMPOSSIBLE
-                return 0;
+                fill_vertical_best(word[i], vertical); // Fill the puzzle grid
+            }
+
+            // When there is only one suitable horizontal space that fit for the word
+            else if (horizontal.occurances == 1 && vertical.occurances == 0)
+            {
+                fill_horizontal(word[i], horizontal); // Fill the puzzle grid
+            }
+
+            // When there is only one suitable vertical space that fit for the word
+            else if (vertical.occurances == 1 && horizontal.occurances == 0)
+            {
+                fill_vertical(word[i], vertical); // Fill the puzzle grid
             }
         }
         checking++; // Incrementing the checking count
     }
-    
+
     // When there were more than one suitable place for a single word
     for (int i = 0; i < word_count; i++)
     {
@@ -259,10 +359,21 @@ int main()
         {
             fill_vertical(word[i], vertical);
         }
+        // When there is no any suitable place for the word
+        else if (horizontal.occurances + vertical.occurances == 0)
+        {
+            printf("IMPOSSIBLE\n"); // Print filling puzzle grid is IMPOSSIBLE
+            return 0;
+        }
     }
 
     // Print final filled puzzle
     print_puzzle(row_count);
 
+    /*// Calculate time taken to execution
+    clock_t end = clock();
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("\nTime Taken = %fs\n", time_spent);
+    */
     return 0;
 }
